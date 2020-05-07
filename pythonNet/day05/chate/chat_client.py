640afa00fc96a2d5f4b69cabf542500401987d14
zhuzhaoming <18327510516@163.com>
2020-05-07 13:24:51 +0800

    finish chat service and client

--- pythonNet/day05/chate/chat_client.py
+++ pythonNet/day05/chate/chat_client.py
@@ -5,8 +5,34 @@ import os, sys
 ADDR = ('0.0.0.0', 8888)
 
 
+# 发送消息
+def send_msg(s, name):
+    while True:
+        try:
+            text = input("发言：")
+        except KeyboardInterrupt:
+            text = 'quit'
+        if text == 'quit':
+            msg = "Q " + name
+            s.sendto(msg.encode(), ADDR)
+            sys.exit("退出聊天室")
+        msg = "C %s %s" % (name, text)
+        s.sendto(msg.encode(), ADDR)
+
+
+# 接收消息
+def recv_msg(s):
+    while True:
+        data, addr = s.recvfrom(2048)
+        # 服务端发送exit表示让客户端退出
+        if data.decode() == 'EXIT':
+            sys.exit()
+        print(data.decode())
+
+
 # 创建网络连接
 def main():
+    # 套接字
     s = socket(AF_INET, SOCK_DGRAM)
     while True:
         name = input("请输入姓名:")
@@ -19,6 +45,15 @@ def main():
             break
         else:
             print(data.decode())
+
+    # 创建新的进程
+    pid = os.fork()
+    if pid < 0:
+        sys.exit("error")
+    elif pid == 0:
+        send_msg(s, name)
+    else:
+        recv_msg(s)
 
 
 if __name__ == '__main__':
--- pythonNet/day05/chate/chat_service.py
+++ pythonNet/day05/chate/chat_service.py
@@ -14,8 +14,9 @@ ADDR = ('0.0.0.0', 8888)
 user = {}
 
 
+# 登陆
 def do_login(s, name, addr):
-    if name in user:
+    if name in user or "管理员" in name:
         s.sendto("该用户已经存在".encode(), addr)
         return
     s.sendto(b'ok', addr)
@@ -26,7 +27,29 @@ def do_login(s, name, addr):
         s.sendto(msg.encode(), user[i])
 
     # 将用户加入
-    user[name] = addr
+    try:
+        user[name] = addr
+    except:
+        print("user[%s]=%s" % (name, user[name]))
+
+
+# 聊天
+def do_chat(s, name, text):
+    msg = "%s :%s" % (name, text)
+    for i in user:
+        if i != name:
+            s.sendto(msg.encode(), user[i])
+
+
+# 退出
+def do_quit(s, name):
+    msg = "%s退出了聊天室" % name
+    for i in user:
+        if i != name:
+            s.sendto(msg.encode(), user[i])
+        else:
+            s.sendto(b"EXIT", user[i])
+    del user[name]
 
 
 # 接受各种客户端请求
@@ -37,7 +60,10 @@ def do_request(s):
         if msg[0] == "L":
             do_login(s, msg[1], addr)
         elif msg[0] == "C":
-            do_chat()
+            text = " ".join(msg[2:])
+            do_chat(s, msg[1], text)
+        elif msg[0] == "Q":
+            do_quit(s, msg[1])
 
 
 # 创建网络连接
@@ -46,12 +72,19 @@ def main():
     s = socket(AF_INET, SOCK_DGRAM)  # UDP套接字
     s.bind(ADDR)
 
+    pid = os.fork()
+    if pid < 0:
+        return
+    elif pid == 0:
+        while True:
+            msg = input("管理员消息：")
+            msg = "C 管理员消息 " + msg
+            s.sendto(msg.encode(), ADDR)
+    else:
+        do_request(s)
+
     # 请求处理
-    do_request(s)  # 处理客户端请求
-
-
-def do_chat():
-    pass
+    # do_request(s)  # 处理客户端请求
 
 
 if __name__ == "__main__":
