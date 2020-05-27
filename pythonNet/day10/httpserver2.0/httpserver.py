5987808a3ff8ee0bd7e3e1ce1ba3bf15c498177f
zhuzhaoming <18327510516@163.com>
2020-05-27 11:04:19 +0800

    httpserver2.0  write  -- small bird study

--- pythonNet/day10/httpserver2.0/httpserver.py
+++ pythonNet/day10/httpserver2.0/httpserver.py
@@ -16,11 +16,90 @@
                     使用类封装
   
 """
+from socket import *
+from select import select
 
 
 # 将具体http server功能封装
 class HTTPServer:
-    pass
+    def __init__(self, server_addr, static_dir):
+        # 添加属性
+        self.server_addr = server_addr
+        self.static_dir = static_dir
+        self.rlist = []
+        self.wlist = []
+        self.xlist = []
+        self.create_socket()
+        self.bind()
+
+    # 创建套接字
+    def create_socket(self):
+        self.sockfd = socket()
+        self.sockfd.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+    def bind(self):
+        self.sockfd.bind(self.server_addr)
+        self.ip = self.server_addr[0]
+        self.port = self.server_addr[1]
+
+    # 启动服务
+    def server_forever(self):
+        self.sockfd.listen(5)
+        print("listen the port %d" % self.port)
+        self.rlist.append(self.sockfd)
+
+        while True:
+            rs, ws, xs = select(self.rlist, self.wlist, self.xlist)
+            for r in rs:
+                if r is self.sockfd:
+                    c, addr = r.accept()
+                    print("connect from ", addr)
+                    self.rlist.append(c)
+                else:
+                    # 处理浏览器请求
+                    self.handler(r)
+
+    # 处理客户端请求
+    def handler(self, connfd):
+        # 接收http请求
+        request = connfd.recv(4096)
+        # 防止浏览器断开
+        if not request:
+            self.rlist.remove(connfd)
+            connfd.close()
+            return
+        # 请求解析
+        request_line = request.splitlines()[0]
+        info = request_line.decode().split(' ')[1]
+        print(connfd.getpeername(), ':', info)
+        # info分为访问网页和其他
+        if info == '/' or info[-5:] == '.html':
+            self.get_html(connfd, info)
+        else:
+            pass
+
+    # 处理网页
+    def get_html(self, connfd, info):
+        if info == '/':
+            # 网页文件
+            filename = self.static_dir + '/index.html'
+        else:
+            filename = self.static_dir + info
+
+        try:
+            fd = open(filename)
+        except Exception:
+            # 没有网页
+            responseHeaders = 'HTTP/1.1 404 NotFound\r\n'
+            responseHeaders += '\r\n'
+            responseBody = "<h1>Sorry ,Not Found the page</h1>"
+        else:
+            responseHeaders = 'HTTP/1.1 200 OK\r\n'
+            responseHeaders += '\r\n'
+            responseBody = fd.read()
+        finally:
+            response = responseHeaders + responseBody
+            connfd.send(response.encode())
 
 
 # 如何使用HTTPServer类
